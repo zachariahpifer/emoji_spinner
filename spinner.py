4d34725576a6eb81d1469a1bf4996530a8957a28
Zachariah Pifer <zachariah.pifer@gentex.com>
2023-05-18 12:26:00 -0400

    top-level function instead of class, docstrings

--- spinner.py
+++ spinner.py
@@ -1,3 +1,4 @@
+"""The module can be imported and used to display spinning animations in the terminal."""
 from typing import Callable
 import os
 from itertools import cycle
@@ -7,25 +8,24 @@ import threading
 import cursor
 
 
-class Spinner:
+class Spinners:
+    """Spinners class contains a few different spinners that can be used"""
     clocks = cycle(["🕛", "🕐", "🕑", "🕒", "🕓", "🕔", "🕕", "🕖", "🕗", "🕘", "🕙", "🕚"])
     moons = cycle(["🌑", "🌒", "🌓", "🌔", "🌕", "🌖", "🌗", "🌘"])
     globes = cycle(["🌍", "🌎", "🌏"])
     circles = cycle(["🔴", "🟠", "🟡", "🟢", "🔵", "🟣", "⚫️", "🟤"])
+    squares = cycle(["⬛️", "🟥", "🟧", "🟨", "🟩", "🟦", "🟪"])
 
-    @staticmethod
+
+def SpinWhile(callback: Callable, *args, symbols: cycle = Spinners.clocks) -> None:
+    """SpinWhile will spin a spinner while the callback function is running
+    thread_wrapper is used to suppress the output of the callback function
+    """
     def thread_wrapper(callback: Callable, *args) -> None:
-        with open(os.devnull, "w") as sys.stdout:
+        with open(os.devnull, "w", encoding="utf-8") as sys.stdout:
             callback(*args)
 
-    @staticmethod
-    def spin(callback: Callable, *args, symbols: cycle = moons) -> None:
-        cursor.hide()
-        terminal = sys.stdout
-        callback_thread = threading.Thread(
-            target=Spinner.thread_wrapper, args=(callback, *args)
-        )
-        callback_thread.start()
+    def spin_loop(symbols, terminal, callback_thread):
         while callback_thread.is_alive():
             terminal.write(next(symbols))
             terminal.flush()
@@ -34,12 +34,21 @@ class Spinner:
             time.sleep(0.25)
         cursor.show()
 
+    cursor.hide()
+    terminal = sys.stdout
+    callback_thread = threading.Thread(
+        target=thread_wrapper, args=(callback, *args)
+    )
+    callback_thread.start()
+    spin_loop(symbols, terminal, callback_thread)
+
 
 def printYForXSeconds(x: int, msg: str) -> None:
+    """printYForXSeconds will print a message y times for x seconds"""
     for _ in range(x):
         print(msg)
         time.sleep(1)
 
 
 if __name__ == "__main__":
-    Spinner.spin(printYForXSeconds, 5, "This wont print", symbols=Spinner.circles)
+    SpinWhile(printYForXSeconds, 5, "This wont print", symbols=Spinners.squares)
